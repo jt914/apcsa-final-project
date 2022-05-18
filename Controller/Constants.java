67c4623b5c0851d0dfa9a8cdaa8282df5573f840
jtso914 <josephtso914@gmail.com>
2022-05-18 18:13:37 -0400

    init

--- Controller/Constants.java
+++ Controller/Constants.java
@@ -1,3 +1,4 @@
+package Controller;
 
 import java.awt.*;
 import java.util.ArrayList;
@@ -9,6 +10,7 @@ public class Constants {
     public static int SW = (int)Toolkit.getDefaultToolkit().getScreenSize().getWidth();
     public static int SH = (int)Toolkit.getDefaultToolkit().getScreenSize().getHeight();
     public static List <Car> cars = new ArrayList<Car>();
+    public static List<String> paths = new ArrayList<String>();
     
 
 
